c4ea2d852bab0e11c18e0e4713ac02beac2319be
lukmandenny <lukmandennylazuardian@gmail.com>
2023-10-27 12:54:21 +0700

    re-add firebase dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,15 @@ import 'package:airplane/ui/pages/bonus_page.dart';
 import 'package:airplane/ui/pages/get_started_page.dart';
 import 'package:airplane/ui/pages/main_page.dart';
 import 'package:airplane/ui/pages/sign_up_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'ui/pages/splash_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
